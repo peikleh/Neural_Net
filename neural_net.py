251507e0bc6cab8ff1ec0abcfcee9aad672c8685
nevinl <nevinleh@gmail.com>
2018-11-08 16:03:35 -0700

    WIP

--- neural_net.py
+++ neural_net.py
@@ -9,7 +9,12 @@ class Network(object):
 		
 		self.weights = [np.random.randn(y, x)
 						for x, y in zip(sizes[:-1], sizes[1:])]
-		print(self.weights)
+		
+		
+	def feedforward(self, a):
+		for b, w in zip(self.biases, self.weights):
+			a = sigmoid(np.dot(w, a)+b)
+		return a
 		
 def read():
 	with open("training.txt") as f:
@@ -22,6 +27,15 @@ def read():
 		values[x][0] = "%06X" % int(values[x][0])
 		values[x][0] = [int(values[x][0][0:2], 16), int(values[x][0][2:4],16), int(values[x][0][4:6],16)]
 	return values
+	
+def sigmoid(z):
+	return 1.0/(1.0+np.exp(-z))
+	
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z)*(1-sigmoid(z))
 		
-net = Network([2, 3, 2])
-read()
+net = Network([3, 1, 2])
+test = read()
+test = np.ndarray(test[0][0])
+print (test)
